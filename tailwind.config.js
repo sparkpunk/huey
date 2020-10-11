7104d7076e552b5d64151317c32d02ca979b9103
Zak Erving <zak@riptideio.com>
2020-10-12 00:51:49 +0200

    setting min/max on hues/shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -124,6 +124,9 @@ module.exports = {
     },
     extend: {
       gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+        '14': 'repeat(14, minmax(0, 1fr))',
+        '15': 'repeat(15, minmax(0, 1fr))',
         '16': 'repeat(16, minmax(0, 1fr))',
         '17': 'repeat(17, minmax(0, 1fr))',
         '18': 'repeat(18, minmax(0, 1fr))',
