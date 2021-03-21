f456d04c520f049f67984ea38b35330ee3f84d53
Zak Erving <zak@riptideio.com>
2021-03-21 13:57:44 +0800

    reordering attrs

--- src/components/ColorInput.vue
+++ src/components/ColorInput.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="relative flex-shrink-0 flex flex-col mb-4">
-    <label :for="name"
-      class="flex justify-between mb-2 ml-px">
+    <label :for="name" class="flex justify-between mb-2 ml-px">
       <span class="text-xs uppercase font-semibold">{{ label }}</span>
-      <span v-if="error" class="text-xs font-semibold text-red-400">Bad value</span>
+      <span v-if="error" class="text-xs font-semibold text-red-400"
+        >Bad value</span
+      >
     </label>
-    <input :id="name"
+    <input
+      :id="name"
       :name="name"
       :type="type"
       :value="value"
       min="2"
-      max="24"
+      max="48"
       @change="validate"
       class="w-full h-10 py-2 px-4 text-sm text-gray-600 bg-transparent border border-gray-300 leading-normal transition-colors duration-100 ease-in-out rounded-lg focus:outline-none select-none truncate focus:bg-white focus:shadow-inner focus:border-gray-400"
-      :class="{'border-red-400': error}" />
+      :class="{ 'border-red-400': error }"
+    />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'color-input',
-  inject: ['handleInput'],
+  name: "color-input",
+  inject: ["handleInput"],
   props: {
     label: {
       type: String,
@@ -58,21 +61,21 @@ export default {
 
       value = type === "number" ? parseInt(value) : value;
 
-      if(name === "hex") {
-        if(value[0] != "#") value = `#${value}`;
+      if (name === "hex") {
+        if (value[0] != "#") value = `#${value}`;
         is_valid = /^(#)?[0-9A-F]{6}$/i.test(value);
       } else {
-        is_valid = true
+        is_valid = true;
       }
 
-      if(is_valid) {
+      if (is_valid) {
         this.error = false;
-        this.handleInput({name, value});
+        this.handleInput({ name, value });
       } else {
         this.error = true;
-        e.srcElement.value = '';
+        e.srcElement.value = "";
       }
     },
-  }
-}
+  },
+};
 </script>
